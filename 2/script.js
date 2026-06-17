79081ea51bb56599d65373d0ef4b0159f7b0bb9f
251ctt032thai-spec <251ctt032_thai@pyu.edu.vn>
2026-06-17 16:02:03 +0700

    Delete 2/script.js

--- 2/script.js
+++
@@ -1,4 +0,0 @@
-const aside = document.querySelector('aside');
-document.querySelector('.toggle-btn').addEventListener('click', () => {
-  aside.style.display = aside.style.display === 'none' ? 'block' : 'none';
-});
